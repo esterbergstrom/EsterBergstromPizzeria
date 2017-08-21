7107c46952a7ed19c58ddd2b7ed4587be6c067bd
Ester Bergström <ester.d.bergstrom@gmail.com>
2017-08-21 10:59:21 +0200

    Added DatabaseInitalizer which adds a mock user to the in-memory database.

---
+++ InMemoryDatabase/Data/DatabaseInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InMemoryDatabase.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace InMemoryDatabase.Data
+{
+    public static class DatabaseInitializer
+    {
+        public static void Initialize(UserManager<ApplicationUser> userManager)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = "jane.doe@gmail.com",
+                Email = "jane.doe@gmail.com"
+            };
+
+            userManager.CreateAsync(user, "Password123!");
+        }
+    }
+}
--- InMemoryDatabase/Startup.cs
+++ InMemoryDatabase/Startup.cs
@@ -35,12 +35,13 @@ namespace InMemoryDatabase
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
+            services.AddTransient<UserManager<ApplicationUser>>();
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, UserManager<ApplicationUser> userManager)
         {
             if (env.IsDevelopment())
             {
@@ -63,6 +64,8 @@ namespace InMemoryDatabase
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DatabaseInitializer.Initialize(userManager);
         }
     }
 }
